8e6af110f0c3f9490a5ca24b5a1f9c7105dd2357
Borisa <borisha.zivkovic@gmail.com>
2013-12-02 20:45:01 +0000

    small fix

--- bauk-core/src/main/java/com/threeglav/bauk/feed/DefaultFeedFileNameProcessor.java
+++ bauk-core/src/main/java/com/threeglav/bauk/feed/DefaultFeedFileNameProcessor.java
@@ -13,7 +13,7 @@ public class DefaultFeedFileNameProcessor implements FeedFileNameProcessor {
 
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	private static final String FEED_FILE_ATTR_PREFIX = "ffAttr_";
+	private static final String FEED_FILE_ATTR_PREFIX = "ffn[";
 
 	private static final String DELIMITER = "_";
 
@@ -25,7 +25,7 @@ public class DefaultFeedFileNameProcessor implements FeedFileNameProcessor {
 			final String[] parsedName = feedFileName.split(DELIMITER);
 			if (parsedName != null && parsedName.length > 0) {
 				for (int i = 0; i < parsedName.length; i++) {
-					parsedFeedFileNameAttributes.put(FEED_FILE_ATTR_PREFIX + i, parsedName[i]);
+					parsedFeedFileNameAttributes.put(FEED_FILE_ATTR_PREFIX + i + "]", parsedName[i]);
 				}
 				log.debug("Attributes parsed from feed file name {} are {}", feedFileName, parsedFeedFileNameAttributes);
 			}
